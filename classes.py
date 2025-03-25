87ef711cf13eaf12021b4a9f5fc9ab993521a33c
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-03-25 09:24:33 -0400

    6

---
+++ classes.py
@@ -0,0 +1,18 @@
+""" class calculator():
+    def add_many(list):
+        print(sum(list))
+        return sum(list)
+    def subtract(list):
+        return list """
+
+""" class Merchant:
+    def _init_(self, name, products):
+        self.name=name
+        self.products=products
+    def sell(self,item):
+        self.products.remove(item)
+        print(self.products)
+Rachel = Merchant("Rachel", ["Apples", "Oranges", "Humans"])
+Kammy = Merchant("Kammy", ["Penguins", "Whales", "Capybaras"])
+print(Rachel.sell("Humans"))
+print(Kammy.sell("Capybaras")) """
