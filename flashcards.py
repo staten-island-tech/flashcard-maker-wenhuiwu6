9b89f3f5c51969784b70f3a2093eed20c4ee9345
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-03-26 09:24:50 -0400

    ss

---
+++ flashcards.py
@@ -0,0 +1,8 @@
+import json
+class User:
+    def __init__(self, name, ):
+        self.name = name
+        self.email = email
+    
+    def display_info(self):
+        return f"User: {self.name}, Email: {self.email}"
