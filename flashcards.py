c25acfe1699b0e3fad97822a9cfe429e57f9decd
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-03-27 09:26:56 -0400

    dd

--- flashcards.py
+++ flashcards.py
@@ -1,8 +1,15 @@
 import json
-class User:
-    def __init__(self, name, ):
-        self.name = name
-        self.email = email
+
+try:
+    with open("FlashCards.json", "r") as file:
+        flash_card = json.load(file)
+except FileNotFoundError:
     
+    flash_card = []
+
+class flashcard:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
     def display_info(self):
-        return f"User: {self.name}, Email: {self.email}"
+        return f"question: {self.question}, answer: {self.answer}"
