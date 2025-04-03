815a6c72e315bc9130151bfd5f714614be840e15
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-04-03 09:26:15 -0400

    Update flashcards.py

--- flashcards.py
+++ flashcards.py
@@ -13,6 +13,49 @@ class flashcard:
         self.key = key
     def display_info(self):
         return f"question: {self.question}, answer: {self.key}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def teacher_mode():
+    flashcards = flash_card()
+    
+    while True:
+        print("\nEnter 'exit' to quit Teacher Mode.")
+        word = input("Enter the word/phrase: ")
+        if word.lower() == 'exit':
+            break
+        answer = input("Enter the answer: ")
+
+        flashcards[word] = answer
+        save_flashcards(flashcards)
+        print(f"Flashcard for '{word}' added successfully!")
+
+    print("\nTeacher Mode has ended.")
+
+
+class Teacher:
+    def __init__(self, question, answer):
+        self.answer = answer
+        self.question = question
+    
+    def display_info(self):
+        return f"Question: {self.question}, Answer: {self.answer}"
     
 class Student:
     def __init__(self, name, answer):
