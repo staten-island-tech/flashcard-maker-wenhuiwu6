9d914f3fda800033aedce629e401299be8019b2a
wenhuiwu6 <wenhuiwu678@gmail.com>
2025-04-22 09:24:22 -0400

    Update flashcards.py

--- flashcards.py
+++ flashcards.py
@@ -1,6 +1,92 @@
 import json
+try:
+    with open("FlashCards.json", 'r') as file:
+        flashcards_data = json.load(file)
+except FileNotFoundError:
+    flashcards_data = []
+except json.JSONDecodeError:
+    flashcards_data=[]
 
-def save_flashcards(flashcards):
+
+
+
+
+
+
+class flashcards:
+    def __init__(card, phrase, answer):
+        card.phrase = phrase
+        card. answer = answer
+    def create_dict(card):
+        print(card.phrase, card.answer)
+        x = {card.phrase : card.answer}
+        return x
+    
+create = input("Do you want to create a new flashcard? (Yes or No): ")
+while create == "Yes" :
+    word_phrase = input("Give word or phrase: ")
+    answer = input("Give answer: ")
+
+    hehe = flashcards(word_phrase,answer)
+    print(hehe.create_dict())
+    
+
+""" def save_flashcards(flashcards):
+    with open("FlashCards.json", 'w') as file:
+        json.dump(flashcards, file, indent=4)
+        print("Saved")
+
+def add_flashcards():
+    try:
+        with open("FlashCards.json", 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+def teacher_mode():
+    flashcards = add_flashcards()
+    while True:
+        word_phrase = input("Enter a word/phrase or 'exit' to leave): ")
+        if word_phrase.lower() == 'exit':
+            break
+        answer = input("Enter the answer: ")
+        flashcards[word_phrase] = answer
+    save_flashcards(flashcards)
+teacher_mode() """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" def save_flashcards(flashcards):
     with open("FlashCards.json", 'w') as file:
         json.dump(flashcards, file, indent=4)
         print("Saved")
@@ -22,7 +108,7 @@ def teacher_mode():
         value = input("Enter the answer: ")
         flashcards[key] = value
     save_flashcards(flashcards)
-    print("Flashcards saved")
+    print("Flashcards saved") """
 
 
 """ def student_mode():
